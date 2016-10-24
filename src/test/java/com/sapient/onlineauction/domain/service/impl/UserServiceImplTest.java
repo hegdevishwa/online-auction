36e49ddb98ab60aeb3e506c5c8c8ea144ea71942
Vishwamitra Hegde <vishwamitra.hegde@sapient.com>
2016-10-24 19:40:40 +0530

    added Mockito test class for User Service

--- src/test/java/com/sapient/onlineauction/domain/service/impl/UserServiceImplTest.java
+++ src/test/java/com/sapient/onlineauction/domain/service/impl/UserServiceImplTest.java
@@ -1,35 +1,51 @@
 package com.sapient.onlineauction.domain.service.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.stub;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.sapient.onlineauction.common.exception.ApplicationException;
+import com.sapient.onlineauction.domain.dao.impl.UserDaoImpl;
+import com.sapient.onlineauction.domain.model.User;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+	@Mock
+	UserDaoImpl userDao;
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	@InjectMocks
+	public UserServiceImpl userService;
 
 	@Test
 	public void testGetUserByKey() {
-		fail("Not yet implemented");
+
+		Optional<User> user = getUser();
+		try {
+			stub(userDao.getUsreByKey(10)).toReturn(user);
+
+			User userRes = userService.getUserByKey(10);
+
+			assertEquals(user.get(), userRes);
+
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Test
@@ -37,14 +53,10 @@ public class UserServiceImplTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testGetUserDao() {
-		fail("Not yet implemented");
-	}
+	private Optional<User> getUser() {
 
-	@Test
-	public void testSetUserDao() {
-		fail("Not yet implemented");
+		return Optional.of(new User("Vishwa", "Hegde", new Date(), "vishwa@sapientr.com", "password", "user", 10));
+
 	}
 
 }
